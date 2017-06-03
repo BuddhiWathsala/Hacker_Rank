f383ff2d23aa6df169d870616dd2c44138e25821
Gothami94 <gothamikarunarathna@gmail.com>
2017-06-03 06:23:53 +0530

    balancedBracket

---
+++ balancedBracket.py
@@ -0,0 +1,30 @@
+def is_matched(expression):
+    
+    flag=1
+    
+    stack=list()
+    openBracket=['(','[','{']
+    closeBracket=[')',']','}']
+    
+    for i in expression:
+        if(i in openBracket):
+            stack.append(i)
+        elif(i in closeBracket):
+            if (len(stack)==0):
+                flag=0
+            else:
+                bracket=stack.pop()
+                if (bracket!=i):
+                    flag=0
+                    
+        return flag
+    
+    #print (expression[0])
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    expression = raw_input().strip()
+    if is_matched(expression) == True:
+        print "YES"
+    else:
+        print "NO"
